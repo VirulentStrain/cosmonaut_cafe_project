037d114b5221f7e96fba5c338748bff4f6d4aa7f
Alex Morgun <aamorgun2003@gmail.com>
2023-01-25 22:09:26 -0700

    Eighth Commit Updated some text

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+/*
+$(function () {
+    $('#fadeInTextOne').hide()
+    $('#firstImage').on({
+        mouseenter: function() {
+            $("#fadeInTextOne").fadeIn(200);
+        },
+        mouseleave: function() {
+            $("#fadeInTextOne").fadeOut(200);        
+        }
+    });
+})
+
+
+$('#firstImage').on('mouseenter', function(){
+        $('#fadeInTextOne').fadeToggle()
+    })
+    */
+
