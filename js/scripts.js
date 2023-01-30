8734a9ccb963ffe1e423f182bc1a5cad4ae213b7
Alex Morgun <aamorgun2003@gmail.com>
2023-01-30 13:04:58 -0700

    Update I added a home and inventory page background image; mixed the dropdown for the inventory page; fixed some aesthetics of all pages; got rid of the cosmonaut cafe logo on the nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,51 @@
-/*
 $(function () {
-    $('#fadeInTextOne').hide()
-    $('#firstImage').on({
-        mouseenter: function() {
-            $("#fadeInTextOne").fadeIn(200);
-        },
-        mouseleave: function() {
-            $("#fadeInTextOne").fadeOut(200);        
-        }
-    });
+    $("#rearrangeBtn").on('click', function () {
+        removeClass()
+        let randomNum =  Math.floor(Math.random() * 3);
+        if (randomNum === 0) {
+            console.log(randomNum)
+            $("#image1").addClass('order-first')
+            $("#image2").addClass('order')
+            $("#image3").addClass('order-last')
+        } else if (randomNum === 1) {
+            console.log(randomNum)
+            $("#image1").addClass('order-last')
+            $("#image2").addClass('order-first')
+            $("#image3").addClass('order')
+        } else if (randomNum === 2) {
+            console.log(randomNum)
+            $("#image1").addClass('order')
+            $("#image2").addClass('order-last')
+            $("#image3").addClass('order-first') 
+        } 
+    function removeClass () {
+        $('#image1').removeClass('order')
+        $('#image1').removeClass('order-first')
+        $('#image1').removeClass('order-last')
+        $('#image2').removeClass('order-first')
+        $('#image2').removeClass('order-last')
+        $('#image2').removeClass('order')
+        $('#image3').removeClass('order')
+        $('#image3').removeClass('order-first')
+        $('#image3').removeClass('order-last')
+        console.log('success')
+    }
+    })
 })
 
 
-$('#firstImage').on('mouseenter', function(){
-        $('#fadeInTextOne').fadeToggle()
-    })
-    */
 
+
+/*
+const reBtn = document.querySelector('#rearrangeBtn')
+const fImage = document.querySelector('#image1')
+const sImage = document.querySelector('#image2')
+const tImage = document.querySelector('#image3')
+
+
+reBtn.addEventListener()
+
+function imageRearrange () {
+
+}
+*/
